ea8b12c19232e451c4fc070620f8ae4780fd3461
MagarAvi99 <157788027+MagarAvi99@users.noreply.github.com>
2025-05-04 22:48:19 +0545

    admindashboard commit

---
+++ src/main/java/com/aptcoursework/controller/servlet/AdminDashboardServlet.java
@@ -0,0 +1,39 @@
+package com.aptcoursework.controller.servlet;
+
+import com.aptcoursework.controller.dao.ProductDAO;
+import com.aptcoursework.controller.dao.UserDAO;
+import com.aptcoursework.model.Product;
+import com.aptcoursework.model.user;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/adminDashboard") // This maps the servlet to the URL /adminDashboard
+public class AdminDashboardServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+            // Create DAO instances
+            UserDAO userDAO = new UserDAO();
+            ProductDAO productDAO = new ProductDAO();
+
+            // Get the total number of users and products
+            int totalUsers = userDAO.getAllUsers().size();
+            int totalProducts = productDAO.getAllProducts().size();
+
+            // Set the attributes to be accessed in the JSP
+            request.setAttribute("totalUsers", totalUsers);
+            request.setAttribute("totalProducts", totalProducts);
+
+            // Forward to adminDashboard.jsp
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/pages/adminDashboard.jsp");
+            dispatcher.forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error retrieving data.");
+        }
+    }
+}
